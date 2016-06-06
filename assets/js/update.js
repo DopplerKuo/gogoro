4da723356b23c36a5b91a799d91efd3a497948d3
DopplerKuo <doppler.kuo@gmail.com>
2016-06-06 18:43:26 +0800

    更新圖片

---
+++ assets/js/update.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {        
+	 // $(".p2-episode01").addClass('comingsoon');
+	 // $(".p2-episode02").addClass('comingsoon');
+	 // $(".p2-episode03").addClass('comingsoon');
+	 // $(".p2-episode04").addClass('comingsoon');
+});
